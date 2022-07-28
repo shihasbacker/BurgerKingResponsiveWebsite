849ee59db7b24c91a7f88e94cacc08322cc70cbc
shihasbacker <shihasbacker@yahoo.com>
2022-07-28 20:26:53 +0530

    bottom button value corrected

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 
 
 function expand() { 
+    var elem = document.getElementById("bkc-button");
     if(document.getElementById('bkc-info').style.display=='none') { 
         document.getElementById('bkc-info').style.display='block'; 
+        elem.value = "Less Details";
+
     }else{
         document.getElementById('bkc-info').style.display = 'none';
-    }
-
+        elem.value = "More Details";  
+    } 
 }
